a948223a1f1b509beeed50e5d36ddffa930a82aa
Curtis Schlak <fuzzygeek1@users.sourceforge.net>
2013-01-24 08:46:29 -0600

    Update to AnotherFakeDbSet v.2

--- FakeDbSet/Properties/AssemblyInfo.cs
+++ FakeDbSet/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MockDbSet")]
+[assembly: AssemblyTitle("AnotherMockDbSet")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("N/A")]
-[assembly: AssemblyProduct("MockDbSet")]
+[assembly: AssemblyProduct("AnotherMockDbSet")]
 [assembly: AssemblyCopyright("Fork it on GitHub")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.0.0")]
-[assembly: AssemblyFileVersion("1.4.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
