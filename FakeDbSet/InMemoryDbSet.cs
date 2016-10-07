97656d02e80016ee94ae61c04fabb95936ab434a
Keiran McDonald <mcdonaldk@ramsayhealth.com.au>
2016-10-07 11:35:47 +1000

    Make InMemoryDbSet inherit from DbSet to allow test doubles to override new methods https://entityframework.codeplex.com/wikipage?title=Design%20Meeting%20Notes%20-%20May%2016%2c%202013

--- FakeDbSet/InMemoryDbSet.cs
+++ FakeDbSet/InMemoryDbSet.cs
@@ -8,21 +8,21 @@ using System.Collections;
 
 namespace FakeDbSet
 {
-	/// <summary>
-	/// The in-memory database set, taken from Microsoft's online example (http://msdn.microsoft.com/en-us/ff714955.aspx) 
-	/// and modified to be based on DbSet instead of ObjectSet.
-	/// </summary>
-	/// <typeparam name="T">The type of DbSet.</typeparam>
-    public class InMemoryDbSet<T> : IDbAsyncEnumerable<T>, IDbSet<T> where T : class
-	{
-		readonly HashSet<T> _data;
-		readonly IQueryable _query;
+    /// <summary>
+    /// The in-memory database set, taken from Microsoft's online example (http://msdn.microsoft.com/en-us/ff714955.aspx) 
+    /// and modified to be based on DbSet instead of ObjectSet.
+    /// </summary>
+    /// <typeparam name="T">The type of DbSet.</typeparam>
+    public class InMemoryDbSet<T> : DbSet<T>, IDbSet<T>, IDbAsyncEnumerable<T>, IQueryable where T : class
+    {
+        readonly HashSet<T> _data;
+        readonly IQueryable _query;
 
-		public InMemoryDbSet() : this(false)
-		{
+        public InMemoryDbSet() : this(false)
+        {
             _data = new HashSet<T>();
-		    _query = _data.AsQueryable();
-		}
+            _query = _data.AsQueryable();
+        }
 
         public InMemoryDbSet(bool clearDownExistingData)
         {
@@ -37,30 +37,40 @@ namespace FakeDbSet
             _data.Clear();
         }
 
-		public T Add(T entity)
-		{
-			_data.Add(entity);
-			return entity;
-		}
+        public override T Add(T entity)
+        {
+            _data.Add(entity);
+            return entity;
+        }
 
-		public T Attach(T entity)
-		{
-			_data.Add(entity);
-			return entity;
-		}
+        public override IEnumerable<T> AddRange(IEnumerable<T> entities)
+        {
+            var items = entities.ToArray();
+            foreach (var item in items)
+            {
+                Add(item);
+            }
+            return items;
+        }
 
-		public TDerivedEntity Create<TDerivedEntity>() where TDerivedEntity : class, T
-		{
-			throw new NotImplementedException();
-		}
+        public override T Attach(T entity)
+        {
+            _data.Add(entity);
+            return entity;
+        }
 
-		public T Create()
-		{
-			return Activator.CreateInstance<T>();
-		}
+        public new TDerivedEntity Create<TDerivedEntity>() where TDerivedEntity : class, T
+        {
+            throw new NotImplementedException();
+        }
 
-		public virtual T Find(params object[] keyValues)
-		{
+        public override T Create()
+        {
+            return Activator.CreateInstance<T>();
+        }
+
+        public new virtual T Find(params object[] keyValues)
+        {
             if (keyValues.Length != 1)
                 throw new Exception("not implemented");
 
@@ -77,39 +87,39 @@ namespace FakeDbSet
                 return this.FirstOrDefault(t => (Guid)getter.Invoke(t, null) == (Guid)keyValues[0]);
             else
                 throw new Exception("unknown keytype");
-		}
+        }
 
-		public System.Collections.ObjectModel.ObservableCollection<T> Local
-		{
-			get { return new System.Collections.ObjectModel.ObservableCollection<T>(_data); }
-		}
+        public override System.Collections.ObjectModel.ObservableCollection<T> Local
+        {
+            get { return new System.Collections.ObjectModel.ObservableCollection<T>(_data); }
+        }
 
-		public T Remove(T entity)
-		{
-			_data.Remove(entity);
-			return entity;
-		}
+        public override T Remove(T entity)
+        {
+            _data.Remove(entity);
+            return entity;
+        }
 
-		public IEnumerator<T> GetEnumerator()
-		{
-			return _data.GetEnumerator();
-		}
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _data.GetEnumerator();
+        }
 
-		IEnumerator IEnumerable.GetEnumerator()
-		{
-			return _data.GetEnumerator();
-		}
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _data.GetEnumerator();
+        }
 
-		public Type ElementType
-		{
-			get { return _query.ElementType; }
-		}
+        public Type ElementType
+        {
+            get { return _query.ElementType; }
+        }
 
-		public Expression Expression
-		{
-			get { return _query.Expression; }
-		} 
-        
+        public Expression Expression
+        {
+            get { return _query.Expression; }
+        }
+
         public IDbAsyncEnumerator<T> GetAsyncEnumerator()
         {
             return new DbAsyncEnumerator<T>(this.AsEnumerable().GetEnumerator());
@@ -124,5 +134,5 @@ namespace FakeDbSet
         {
             get { return new DbAsyncQueryProvider<T>(_data.AsQueryable().Provider); }
         }
-	}
+    }
 }
